ee104f39f008fc19974ea34a82c3462ee2c2cd9e
KMatsun <77029134+KMatsun@users.noreply.github.com>
2021-01-13 23:04:23 +0900

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import matplotlib as mt
 st.title("てすとぺ一じ")
 st.write("サンプルデータ")
 
-df=pd.read_csv('/data/data/com.termux/files/home/salesdata/sample.csv',index_col=0)
+df=pd.read_csv(https://github.com/KMatsun/salesdatatest/sample.csv',index_col=0)
 
 
 st.write(df)
