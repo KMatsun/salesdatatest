99fa56ae59557df9c7c687ae5168854d0c29a1af
KMatsun <sitropsis2046@gmail.com>
2021-02-01 06:27:21 +0900

    add-date4

--- test3.py
+++ test3.py
@@ -11,7 +11,7 @@ url="https://drive.google.com/uc?id=11CUOWKfXvZDDGmgbkg4Ht5Kwxgk-mVh3"
 
 df=pd.read_csv(url,index_col=0)
 
-pd.to_datetime(df['date'])
+df(['date'])=pd.to_datetime(df['date'])
 
 df["SRcum"]=df["result"].cumsum()
 st.line_chart(df["SRcum"])
