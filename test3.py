21acd8d5cd78a4196f70b5512676e8adf5331c06
KMatsun <sitropsis2046@gmail.com>
2021-02-01 18:56:45 +0900

    add-line7

--- test3.py
+++ test3.py
@@ -18,7 +18,8 @@ df["SRcum"]=df["result"].cumsum()
 df["targetcum"]=df["target"].cumsum()
 df["PYcum"]=df["PY"].cumsum()
 
-st.line_chart(df.loc['date','SRcum','targetcum','PYcum'])
+st.line_chart(df.loc['SRcum','targetcum','PYcum'])
+
 
 
 window = 7
