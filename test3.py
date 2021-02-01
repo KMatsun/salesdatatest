c28884b5161b062dafb0bd2b38dc9309eff92bde
KMatsun <sitropsis2046@gmail.com>
2021-02-01 20:39:29 +0900

    add-datetype6

--- test3.py
+++ test3.py
@@ -9,7 +9,7 @@ st.write("Sales")
 
 url="https://drive.google.com/uc?id=11CUOWKfXvZDDGmgbkg4Ht5Kwxgk-mVh3"
 
-df=pd.read_csv(url,index=['date'],parse_dates=['date'])
+df=pd.read_csv(url,set_index=['date'],parse_dates=['date'])
 df['date']=pd.to_datetime(df['date'],format='%Y%m%d')
 
 
